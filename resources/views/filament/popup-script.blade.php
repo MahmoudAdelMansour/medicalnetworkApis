1737a71647776ac83e7d46e279264050877e1995
mahmoudadelali <mssmahmoud4@gmail.com>
2025-10-03 02:53:16 +0300

    Flower

--- resources/views/filament/popup-script.blade.php
+++ resources/views/filament/popup-script.blade.php
@@ -1,16 +1,15 @@
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         // Check if popup has been shown in this session
-        if (sessionStorage.getItem('popupShown')) {
+
             setTimeout(() => {
                 new FilamentNotification()
                     .title('Made with Love ❤️ For The Flutter 🌹')
                     .success()
                     .duration(5000)
                     .send();
-
                 sessionStorage.setItem('popupShown', 'true');
             }, 500);
-        }
+
     });
 </script>
